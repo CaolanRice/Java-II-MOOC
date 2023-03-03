e9a062178e845cbe8ece8ed0c3b601d84614aad3
CaolanRice <103444357+CaolanRice@users.noreply.github.com>
2023-03-03 22:31:18 +0100

    Add files via upload

---
+++ Part-12 (Type parameters + Multidimensional data)/part12-Part12_05.HashMap/src/main/java/Program.java
@@ -0,0 +1,12 @@
+
+
+
+public class Program {
+
+    public static void main(String[] args) {
+        Pair<String, Integer> pairs = new Pair<>("Hey", 2);
+        System.out.println(pairs.getKey() + " ---> " + pairs.getValue());
+
+    }
+
+}
